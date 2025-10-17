e7de2a80b7ee5e055edb25f088dba836f8790525
Prince Dedha <princededha59@gmail.com>
2025-10-17 23:09:01 +0530

    Add Python program to remove all spaces from a string
    Created a Python script that removes all spaces from a user-entered sentence.
    The program iterates through each character in the string, appends non-space characters
    to a new string, and prints the result.
    
    Demonstrates:
    - String manipulation
    - Loops
    - Conditional statements in Python

---
+++ python-basics/loops/remove_all_spaces.py
@@ -0,0 +1,8 @@
+# Remove All Spaces
+# Ask the user to enter a sentence. Print it without any spaces.
+str1=input("Enter a sentence: ")
+str2=""
+for i in str1:
+    if i !=" ":
+        str2+=i
+print(str2)
